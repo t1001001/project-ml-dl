12b1525827e8725d916535404a634b87081f8d22
t1001001 <tobias1001001@proton.me>
2025-11-30 21:27:52 +0100

    Changed requirements.txt

---
+++ src/data_prep/data_augmentation.py
@@ -0,0 +1,10 @@
+import os
+import cv2
+import random
+import numpy as np
+from pathlib import Path
+from tqdm import tqdm
+from PIL import Image, ImageEnhance
+import torchvision.transforms as transforms
+import utils.config as conf
+
