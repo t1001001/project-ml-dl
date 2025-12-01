d9d9790ec78a9f5286db6c52526bac437683fdd5
t1001001 <tobias1001001@proton.me>
2025-12-01 22:20:33 +0100

    Removed unused imports from main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,8 @@
-import os
 import utils.config as conf
 from utils.cleanup import cleanup
 from data_prep.get_dataset import get_dataset
 from data_prep.split_dataset import split_dataset
 from data_prep.data_augmentation import process_images
-from models.alexnet import train_alexnet
-from models.mobilenetv2 import train_mobilenetv2
-from models.resnet50 import train_resnet50
-from evaluation.alexnet_eval import evaluate_alexnet
-from evaluation.mobilenetv2_eval import evaluate_mobilenetv2
-from evaluation.resnet50_eval import evaluate_resnet50
 
 TRAIN_FOLDERS = [
     conf.DATASET_TRAIN_BUTTERFLY_PATH,
